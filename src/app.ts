dd3aa791f92b24449d9eb703dba2a7c8c442087b
PedroGaya <pedrohenriquegaya@gmail.com>
2024-09-24 13:49:06 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,10 @@ import { tags } from "./routes/tags";
 import { series } from "./routes/series";
 import { NODE_ENV } from "./constants";
 
+import cors from "@elysiajs/cors";
+
 export const app = new Elysia()
+  .use(cors())
   .onRequest(({ request }) => {
     if (NODE_ENV == "development") console.log(request.method, request.url);
   })
